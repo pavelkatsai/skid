674ea429ecc953ffbb69da42dde61adf2d09e7d4
pavelkatsai <87274597+pavelkatsai@users.noreply.github.com>
2021-07-13 15:12:08 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+let user_name = prompt("Введите ваше имя: ");
+alert(user_name);
+
+let scores = 0;
+
+let firstQ = prompt("Выберите язык разметки: a) HTML b) CSS c) JS ");
+if (firstQ == "a"){
+    scores +=2; // scores = scores +2
+}
+alert(`${scores} - результат!`)
+let mz = prompt("Выберите какой сейчас год : a) 2020 b) 2023 c) 2021 ");
+if (mz == "c"){
+    scores +=2; // scores = scores +2
+}
+alert(`${scores} - результат!`)
+let dz = prompt("Выберите создателя теслы: a) Ilon Mask b) Trump c) Ya ");
+if (dz == "a"){
+    scores +=2; // scores = scores +2
+}
+alert(`${scores} - результат!`)
+let nz = prompt("Выберите как звали кролика в смешариках?: a) Лосяш b) Крош c) Барашик ");
+if (nz == "b"){
+    scores +=2; // scores = scores +2
+}
+alert(`${scores} - результат!`)
+let qz = prompt("Выберите как называли зелёного монстра в марвел?: a) Halk b) Captain.A c) Loki ");
+if (qz == "a"){
+    scores +=2; // scores = scores +2
+}
+alert(`${scores} - результат!`)
